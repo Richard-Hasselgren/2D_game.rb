5aa38f06397533ae0ca149ee9859e904c5bcfb4e
Richard Hasselgren <richard.hasselgren@elev.it-gymnasiet.se>
2012-12-10 15:38:53 +0100

    shop

---
+++ shop.rb
@@ -0,0 +1,34 @@
+class Customer
+
+	attr_reader :name, :cart
+
+	def initialize(name)
+		@name = name
+		@cart = Cart.new(self)
+	end
+
+end
+
+class Cart
+
+	attr_reader :customer, :item
+	
+	def initialize(customer)
+		@customer = customer
+		@items = []
+	end
+
+end
+
+
+class Item
+
+	def initialize
+	end
+
+ end
+
+cust1 = Customer.new("Daniel")
+cust2 = Customer.new("Eddie")
+puts cust1.name
+puts cust2.name
