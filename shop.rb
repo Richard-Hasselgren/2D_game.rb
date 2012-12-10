34f9c2391e54cfe5fb16073e0efd44e0683b901d
Richard Hasselgren <richard.hasselgren@elev.it-gymnasiet.se>
2012-12-10 15:42:35 +0100

    update v.2 shop

--- shop.rb
+++ shop.rb
@@ -32,5 +32,8 @@ class Item
 
 cust1 = Customer.new("Daniel")
 cust2 = Customer.new("Eddie")
+korv = Item.new("sausage", 12)
+senap = Item.new("mustard", 5)
+mos = Item.new("mash", 3)
 puts cust1.name
 puts cust2.name
